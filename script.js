893b372ca1035053442c036fff2edf5e8b3a0b67
Afril <upgradeskills223@gmail.com>
2026-04-14 21:30:21 +0800

    first commit portofolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Back-End Developer", "The Creator", "The Engginer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
