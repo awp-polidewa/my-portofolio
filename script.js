8012830896d8f54d1b7571889da5b669096bebb5
Afril <upgradeskills223@gmail.com>
2026-04-16 00:37:12 +0800

    update fitur auto play musik

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// text berjalan
 var typed = new Typed(".multiple-text", {
   strings: ["Back-End Developer", "The Creator", "The Engginer"],
   typeSpeed: 100,
@@ -6,9 +7,24 @@ var typed = new Typed(".multiple-text", {
   loop: true,
 });
 
+// menu togle
 const menuToggle = document.querySelector(".menu-toggle input");
 const navbar = document.querySelector(".navbar");
 
 menuToggle.addEventListener("click", function () {
   navbar.classList.toggle("slide");
 });
+
+// music
+const music = document.getElementById("bgMusic");
+const btn = document.getElementById("btnMusic");
+
+function toggleSound() {
+  if (music.paused) {
+    music.play();
+    btn.textContent = "Stop music";
+  } else {
+    music.pause();
+    btn.textContent = "Play music";
+  }
+}
